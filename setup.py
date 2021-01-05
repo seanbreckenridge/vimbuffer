d9af445a13785cf2eb1357f4d68bf97203323e16
Sean Breckenridge <seanbrecke@gmail.com>
2021-01-04 23:35:55 -0800

    gitlab -> github

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with io.open("README.md", encoding="utf-8") as fo:
 setup(
     name="vimbuffer",
     version="0.1.3",
-    url="https://gitlab.com/seanbreckenridge/vimbuffer",
+    url="https://github.com/seanbreckenridge/vimbuffer",
     author="Sean Breckenridge",
     author_email="seanbrecke@gmail.com",
     description="Edit files, and strings in temporary vim (or some other console editor) buffers.",
